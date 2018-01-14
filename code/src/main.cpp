92abeaa5cea2a515171df3a205b7062860c1b8f7
sdeghuee <sdeghuee@gmail.com>
2018-01-14 18:23:56 -0500

    Updated code for final version of mini cactuscup

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,108 +1,191 @@
-// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
-// // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
-
 #include <Adafruit_NeoPixel.h>
 
-// Which pin on the Arduino is connected to the NeoPixels?
-// On a Trinket or Gemma we suggest changing this to 1
-#define PIN            A6
+#define PIN A6
+#define SWI 6
+#define NUMPIXELS 23
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(60, PIN, NEO_GRB + NEO_KHZ800);
 
-// How many NeoPixels are attached to the Arduino?
-#define NUMPIXELS      20
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
+void colorWipe(uint32_t,uint8_t);
+void rainbow(uint8_t);
+void rainbowCycle(uint8_t);
+void theaterChase(uint32_t,uint8_t);
+void theaterChaseRainbow(uint8_t);
+uint32_t Wheel(byte);
 void bruce(int);
-int val = 1;
+void dash(int);
+void dot(int);
+void allone(int,int, int);
+int phase = 0;
+
+
 void setup() {
+    // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+    pinMode(SWI,INPUT);
+    strip.begin();
+    strip.show(); // Initialize all pixels to 'off'
+    strip.setBrightness(20);
     Serial.begin(9600);
-    pixels.begin(); // This initializes the NeoPixel library.
-    pixels.setBrightness(50);
-    Serial.begin(9600);
-    pinMode(A2,OUTPUT);
 }
 
 void loop() {
-    digitalWrite(A2,HIGH);
-    // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
-    // fix 2, 13
-        val = Serial.parseInt();
-        pixels.setPixelColor(val,pixels.Color(100,0,0));
-        pixels.show();
-        delay(10);
-
-}
-
-// Function to shift the piece of text in bruce across the 23 leds
-// a long bar is R+G, a dot is R followed by a blue
-// LOGIC SHOULD BE FIXED TO ALLOW FOR ACTUAL LONG DOTS
-void bruce(int loops) {
-    unsigned int bruce = 1576367959;
-    unsigned int shiftedBruce = bruce;
-    unsigned int current = 0;
-    int previous =0;
-    for(int i =0; i < NUMPIXELS;i++)
+    // Some example procedures showing how to display to the pixels:
+//    bruce(500);
+//    colorWipe(strip.Color(255, 0, 0), 50); // Red
+//    colorWipe(strip.Color(0, 255, 0), 50); // Green
+//    colorWipe(strip.Color(0, 0, 255), 50); // Blue
+//    colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+    // Send a theater pixel chase in...
+//    theaterChase(strip.Color(127, 127, 127), 50); // White
+//    theaterChase(strip.Color(127, 0, 0), 50); // Red
+//    theaterChase(strip.Color(0, 0, 127), 50); // Blue
+//    rainbow(20);
+    phase = !digitalRead(SWI);
+    if(phase == 0)
     {
-        current=shiftedBruce&(1<<i);
-        if(current)
-        {
-            current = current / current;
-        }
-        pixels.setPixelColor(i,pixels.Color(255*current,0,0));
-        Serial.print(i);
-        Serial.print(" ");
-        Serial.println(current);
+        allone(0,0,0);
     }
-    pixels.show();
-    delay(250);
+    else if(phase == 1)
+    {
+        rainbowCycle(20);
+    }
+//    theaterChaseRainbow(50);
+    
 }
-/*
-        for(int n = 0;n<30;n++)
-        {
 
-            rCur = bruce & 1;
-            gCur = bruce & 2;
-            bCur = ~(rCur & gCur);
-            for(int i=0;i<NUMPIXELS-1;i++){
-                pixels.setPixelColor(i, pixels.Color(rCur*255,gCur*255,bCur*255));
-                shiftedBruce = shiftedBruce << 2;
-                pixels.setPixelColor(i+1, pixels.Color(rCur*255,gCur*255,bCur*255));
-                pixels.show();
-                delay(500);
+// Fill the dots one after the other with a color
+void colorWipe(uint32_t c, uint8_t wait) {
+    for(uint16_t i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, c);
+        strip.show();
+        delay(wait);
+    }
+}
+
+void rainbow(uint8_t wait) {
+    uint16_t i, j;
+
+    for(j=0; j<256; j++) {
+        for(i=0; i<strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel((i+j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+// Slightly different, this makes the rainbow equally distributed throughout
+void rainbowCycle(uint8_t wait) {
+    uint16_t i, j;
+
+    for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+        for(i=0; i< strip.numPixels(); i++) {
+            strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+        }
+        strip.show();
+        delay(wait);
+    }
+}
+
+//Theatre-style crawling lights.
+void theaterChase(uint32_t c, uint8_t wait) {
+    for (int j=0; j<10; j++) {  //do 10 cycles of chasing
+        for (int q=0; q < 3; q++) {
+            for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, c);    //turn every third pixel on
+            }
+            strip.show();
+
+            delay(wait);
+
+            for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, 0);        //turn every third pixel off
             }
         }
-        shiftedBruce = bruce;
     }
-*/
+}
 
+//Theatre-style crawling lights with rainbow effect
+void theaterChaseRainbow(uint8_t wait) {
+    for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+        for (int q=0; q < 3; q++) {
+            for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+            }
+            strip.show();
 
-//BEAT DETECTION
-//Reads in frequency bands and displays the lights as appropriate 
+            delay(wait);
 
-void beats() {
-    int bass = 0;
-    int mids = 0;
-    int highs= 0;
-    bass = analogRead(4);
-    mids = analogRead(8);
-    highs= analogRead(9);
-
-    bass = map(bass, 0,4095,0,255);
-    mids = map(mids, 0,4095,0,255);
-    highs =map(highs,0,4095,0,255);
-
-    for(int i =0;i<NUMPIXELS;i++){
-        pixels.setPixelColor(i, pixels.Color(highs,mids,bass));
+            for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+                strip.setPixelColor(i+q, 0);        //turn every third pixel off
+            }
+        }
     }
-    //Toggle for reset of reading
-    digitalWrite(15,LOW);
-    digitalWrite(17,LOW);
-    digitalWrite(18,LOW);
-    delay(1);
-    digitalWrite(15,HIGH);
-    digitalWrite(17,HIGH);
-    digitalWrite(18,HIGH);
-    delay(250);
+}
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+        return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+    }
+    if(WheelPos < 170) {
+        WheelPos -= 85;
+        return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+    }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+}
+
+void bruce(int time)
+{
+    dash(time);
+    dot(time);
+    dot(time);
+    dot(time);
+
+    dot(time);
+    dash(time);
+    dot(time);
+
+    dot(time);
+    dot(time);
+    dash(time);
+
+    dash(time);
+    dot(time);
+    dash(time);
+    dot(time);
+
+    dot(time);
+}
+
+void dash(int time)
+{
+    Serial.println("dash");
+    allone(0,255,0);
+    delay(2*time);
+    allone(0,0,0);
+    delay(time);
+}
+
+void dot(int time)
+{
+    Serial.println("dot");
+    allone(0,255,0);
+    delay(time);
+    allone(0,0,0);
+    delay(time);
+}
+
+void allone(int g,int r, int b)
+{
+    for(uint16_t i=0; i<strip.numPixels(); i++)
+    {
+        strip.setPixelColor(i, strip.Color(g, r, b));
+        strip.show();
+    }
 }
